6da66c29b6d41c2fe5162b359e85be3067fae0b2
Lucas Croslyn <croslynlc@gmail.com>
2025-04-22 20:42:05 -0300

    Fix up the score function/file

--- score.py
+++ score.py
@@ -1,31 +1,15 @@
-
-import sys
-
 def MSE(preds, golds):
+    '''
+    Calculates the mean squared error between a list of predictions and their true, gold values
+
+    :param preds: A list of the predictions that can be easily formatted as a float.
+    :param golds: A list of what the predictions should give in the best case scenario and can be easily formatted into floats.
+    :return: Returns the mean squared error over the entire list of data.
+    '''
+    
+    assert len(preds) == len(golds), "Different number of predictions and golds"
     curr_MSE = 0
     for i in range(len(preds)):
         curr_MSE += (float(preds[i])-float(golds[i]))**2
     curr_MSE = float(curr_MSE)/float(len(golds))
     return curr_MSE
-
-if __name__ == '__main__':
-    import numpy as np
-
-    data = np.load('Y_test.npy')
-    print(data)
-
-
-    # predictions_fname = sys.argv[1]
-    # gold_standard_fname = sys.argv[2]
-    #
-    # predictions = [x.strip() for x in open(predictions_fname,
-    #                                        encoding='utf8')]
-    #
-    # golds = [x.strip() for x in open(gold_standard_fname,
-    #                                  encoding='utf8')]
-    #
-    # assert len(predictions) == len(golds), "Different number of predictions and golds"
-    # print('MSE: ', str(MSE(predictions, golds)))
-
-
-
